49711f91b0d7cf6c2946214e7746e173d80dd5a4
fullacc <fullaccepted@gmail.com>
2019-06-09 22:29:18 +0600

    rename all

---
+++ week1/if_else2.py
@@ -0,0 +1,20 @@
+def ifelse(n):
+
+    used = 0
+    if n % 2 == 1:
+        used = 1
+    else:
+        if n > 5 and n < 21:
+            used = 1
+
+    if used == 1:
+        print("Weird")
+    else:
+        print("Not Weird")
+
+def main():
+    n = int(input())
+    ifelse(n)
+
+if __name__ == '__main__':
+    main()
---
+++ week1/test/2test.py
@@ -0,0 +1,4 @@
+import pytest
+
+from ..if_else2 import *
+
