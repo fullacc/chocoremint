9e44a710bee9a5b89ddd758058161c047b125069
fullacc <fullaccepted@gmail.com>
2019-06-10 00:10:29 +0600

    added tests for 12

--- week1/tests/12test.py
+++ week1/tests/12test.py
@@ -3,7 +3,8 @@ from io import StringIO
 from ..ginorts12 import ginorts
 
 test1 = StringIO("Sorting1234\n")
-
+test2 = StringIO("Chocofood1112\n")
+test3 = StringIO("\n")
 
 def test_case_1(monkeypatch):
     monkeypatch.setattr("sys.stdin", test1)
@@ -12,3 +13,9 @@ def test_case_1(monkeypatch):
 
 def test_case_2(monkeypatch):
     monkeypatch.setattr("sys.stdin", test2)
+    assert ginorts() == "cdfhooooC1112"
+
+
+def test_case_3(monkeypatch):
+    monkeypatch.setattr("sys.stdin", test3)
+    assert ginorts() == ""
