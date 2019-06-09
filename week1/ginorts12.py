dd7f20518fde85e1512177a43cb681852cb0368c
fullacc <fullaccepted@gmail.com>
2019-06-10 00:06:37 +0600

    ginortstest

--- week1/ginorts12.py
+++ week1/ginorts12.py
@@ -1,25 +1,36 @@
 import re
 
-to_sort = input()
 
-lower_case = re.findall(r"[a-z]", to_sort)
-upper_case = re.findall(r"[A-Z]", to_sort)
+def ginorts():
+    to_sort = input()
 
-numbers = re.findall(r"[0-9]", to_sort)
+    lower_case = re.findall(r"[a-z]", to_sort)
+    upper_case = re.findall(r"[A-Z]", to_sort)
 
-evens = []
-odds = []
+    numbers = re.findall(r"[0-9]", to_sort)
 
-for i in numbers:
-    if int(i) % 2 == 0:
-        evens.append(i)
-    else:
-        odds.append(i)
+    evens = []
+    odds = []
 
-lower_case.sort()
-upper_case.sort()
-odds.sort()
-evens.sort()
+    for i in numbers:
+        if int(i) % 2 == 0:
+            evens.append(i)
+        else:
+            odds.append(i)
 
-result = "".join(x for x in lower_case + upper_case + odds + evens)
-print(result)
+    lower_case.sort()
+    upper_case.sort()
+    odds.sort()
+    evens.sort()
+
+    result = "".join(x for x in lower_case + upper_case + odds + evens)
+    print(result)
+    return result
+
+
+def main():
+    ginorts()
+
+
+if __name__ == '__main__':
+    main()
---
+++ week1/tests/12test.py
@@ -0,0 +1,9 @@
+import pytest
+from io import StringIO
+from ..ginorts12 import ginorts
+
+test1 = StringIO('Sorting1234\n')
+
+def test_case_1(monkeypatch):
+    monkeypatch.setattr('sys.stdin', test1)
+    assert ginorts() == "ginortS1324"
