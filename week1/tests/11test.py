19d4b4552f1da9d90a803ece248fd5241f58651e
fullacc <fullaccepted@gmail.com>
2019-06-10 01:03:18 +0600

    updated some tests

--- week1/tests/11test.py
+++ week1/tests/11test.py
@@ -3,8 +3,20 @@ from io import StringIO
 from ..no_idea11 import noidea
 
 test1 = StringIO("3 2\n1 5 3\n3 1\n5 7\n")
+test2 = StringIO("3 2\n1 5 3\n3 1\n5 7\n")
+test3 = StringIO("3 2\n1 5 3\n3 1\n5 7\n")
 
 
 def test_case_1(monkeypatch):
     monkeypatch.setattr("sys.stdin", test1)
+    assert noidea() == 1
+
+
+def test_case_2(monkeypatch):
+    monkeypatch.setattr("sys.stdin", test2)
+    assert noidea() == 1
+
+
+def test_case_3(monkeypatch):
+    monkeypatch.setattr("sys.stdin", test3)
     assert noidea() == 1
--- week1/tests/7test.py
+++ week1/tests/7test.py
@@ -4,8 +4,8 @@ from ..validating_credit_card_number7 import *
 test1 = StringIO(
     "6\n4123456789123456\n5123-4567-8912-3456\n61234-567-8912-3456\n4123356789123456\n5133-3367-8912-3456\n5123 - 3567 - 8912 - 3456\n"
 )
-test2 = StringIO("44\n")
-
+test2 = StringIO("1\n8278929\n")
+test3 = StringIO("1\n7727298200209\n")
 
 def test_case_1(monkeypatch):
     monkeypatch.setattr("sys.stdin", test1)
@@ -14,4 +14,9 @@ def test_case_1(monkeypatch):
 
 def test_case_2(monkeypatch):
     monkeypatch.setattr("sys.stdin", test2)
-    assert main() == True
+    assert validate() == ["Invalid"]
+
+
+def test_case_3(monkeypatch):
+    monkeypatch.setattr("sys.stdin", test3)
+    assert validate() == ["Invalid"]
