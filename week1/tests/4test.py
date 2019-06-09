8711bccc4819dd6fe40ff4738b8faaf286224144
fullacc <fullaccepted@gmail.com>
2019-06-10 00:51:45 +0600

    Update 4test.py

--- week1/tests/4test.py
+++ week1/tests/4test.py
@@ -3,6 +3,7 @@ from ..division4 import *
 
 test1 = StringIO("4\n3\n")
 test2 = StringIO("4\n5\n")
+test3 = StringIO("54\n66\n")
 
 
 def test_case_1(monkeypatch):
@@ -13,3 +14,8 @@ def test_case_1(monkeypatch):
 def test_case_2(monkeypatch):
     monkeypatch.setattr("sys.stdin", test2)
     assert main() == (0, 0.8)
+
+
+def test_case_3(monkeypatch):
+    monkeypatch.setattr("sys.stdin", test3)
+    assert main() == (0, 0.8181818181818182)
